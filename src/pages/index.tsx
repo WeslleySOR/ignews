21cd7330f96fc330f3457982efb7831829c9eb1a
WeslleySOR <weslleywruas@gmail.com>
2021-11-29 12:08:50 -0300

    add global styles and remove home.module.scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import style from '../styles/home.module.scss'
-
 import Head from 'next/head'
 
 export default function Home() {
@@ -8,7 +6,7 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1 className={style.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 }

e8f4cf249bac52b9e85a3aaea9ae7c7c4528fbc2
WeslleySOR <weslleywruas@gmail.com>
2021-11-29 11:53:29 -0300

    add title dinâmico por pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import style from '../styles/home.module.scss'
 
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1 className={style.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={style.title}>Hello World</h1>
+    </>
   )
 }

131cfa45bb9b7da749fe4650e991091ef7560ab9
WeslleySOR <weslleywruas@gmail.com>
2021-12-11 09:58:14 -0300

    API routes no Next.js

---
+++ src/pages/api/users.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    const users = [
+        { id: 1, name: 'Weslley' }
+    ]
+    return response.json(users);
+}

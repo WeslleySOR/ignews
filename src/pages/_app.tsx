050002006a3dcbe547fdbfd9a4f87912d56664ba
WeslleySOR <weslleywruas@gmail.com>
2022-02-11 16:46:56 -0300

    Revert "finish"
    This reverts commit 9bc74ab0a10e73da279d6a2b428889777d99467c.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { AppProps } from 'next/app'
-import { Header } from '../components/Header';
-import { Provider as NextAuthProvider } from 'next-auth/client'
 
-import '../styles/global.scss';
+import { SessionProvider } from 'next-auth/react'
+
+import { Header } from '../components/Header'
+
+import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextAuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </NextAuthProvider>
+    </SessionProvider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/Head';
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
+import { GetStaticProps } from 'next'
 
-import styles from './home.module.scss';
+import Head from 'next/head'
+import { SubscribeButton } from '../components/Header/SubscribeButton'
+import { stripe } from '../services/stripe'
+
+import styles from './home.module.scss'
 
 interface HomeProps {
   product: {
@@ -18,17 +19,17 @@ export default function Home({ product }: HomeProps) {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
-          <p>Get access to all the publications <br />
+          <p>Get acess to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
           <SubscribeButton priceId={product.priceId} />
         </section>
-
-        <img src="/images/avatar.svg" alt="girl coding." />
+        <img src="/images/avatar.svg" alt="Girl Coding" />
       </main>
     </>
   )
@@ -44,10 +45,11 @@ export const getStaticProps: GetStaticProps = async () => {
       currency: 'USD'
     }).format(price.unit_amount / 100),
   }
+
   return {
     props: {
       product,
     },
-    revalidate: 60 * 60 * 24, //24 hours
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }

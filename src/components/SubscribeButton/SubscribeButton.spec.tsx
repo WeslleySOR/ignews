766f749a351d2f1884cee63f5634a6344c047459
WeslleySOR <weslleywruas@gmail.com>
2022-02-13 21:00:41 -0300

    Testando getStaticProps

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -4,9 +4,12 @@ import { SubscribeButton } from '.'
 import { useRouter } from 'next/router'
 import { signIn, useSession } from 'next-auth/react'
 import { mocked } from 'jest-mock'
+import { stripe } from '../../services/stripe'
+import { getStaticProps } from '../../pages'
 
 jest.mock('next-auth/react')
 jest.mock('next/router')
+jest.mock('../../services/stripe')
 
 describe('SubscribeButton Component', () => {
     it('renders correctly', () => {
@@ -73,5 +76,26 @@ describe('SubscribeButton Component', () => {
 
         expect(pushMock).toHaveBeenCalledWith('/posts')
 
+    })
+
+    it('loads initial data', async () => {
+        const retrieveStripePricesMocked = mocked(stripe.prices.retrieve)
+        retrieveStripePricesMocked.mockResolvedValueOnce({
+            id: 'fake-price-id',
+            unit_amount: 1000,
+        } as any)
+
+        const response = await getStaticProps({})
+
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    product: {
+                        priceId: 'fake-price-id',
+                        amount: '$10.00'
+                    }
+                }
+            })
+        )
     })
 })
